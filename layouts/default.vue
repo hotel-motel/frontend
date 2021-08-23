361c7733f36ce21ca8acb2e725574ee9c91811a5
Omid Reza <snip77.orh@gmail.com>
2021-08-23 21:29:44 +0430

    changelogs : 1. add app.html 2. add some layout

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+<!--    <HeaderMain v-if=" ! $auth.loggedIn"/>-->
+<!--    <HeaderMainAuthed v-if="$auth.loggedIn" />-->
+    <Nuxt />
+<!--    <FooterMain />-->
+  </div>
+</template>
