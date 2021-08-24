aa8d862a4e22211bf9fbaf1f4c532d1be05b4708
Omid Reza <snip77.orh@gmail.com>
2021-08-24 20:48:53 +0430

    load cities from api and show it

--- pages/cities/index.vue
+++ pages/cities/index.vue
@@ -1,10 +1,30 @@
 <template>
-
+  <div class="grid gap-3 container">
+    <span class="h3 text-light">
+      Cities
+    </span>
+    <div class="row justify-around">
+      <div class="col-4 mt-2 mb-2" v-for="city in cities">
+        <City :name="city.name" :image="city.image"></City>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  auth:false
+  auth:false,
+  data(){
+    return{
+      cities:[]
+    }
+  },
+  mounted() {
+    this.$nextTick(()=>{
+      this.$api.$get('/cities')
+      .then(response=>this.cities=response)
+    })
+  }
 }
 </script>
 
