dc0e7c4a96afc8b518be5f70309b5d2b44539c20
Omid Reza <snip77.orh@gmail.com>
2021-08-27 23:28:25 +0430

    Update page content

--- pages/dashboard/personal-info.vue
+++ pages/dashboard/personal-info.vue
@@ -1,15 +1,70 @@
 <template>
   <div class="text-light">
-    personal info
+    <div class="d-flex justify-content-center mt-1 mb-1" v-if="is_loading">
+      <div class="spinner-border" role="status"></div>
+    </div>
+    <div class="grid gap-7 h5" v-else-if="user">
+      <div class="row">
+        <div class="col">
+          <i class='bx bx-user-pin bx-sm'></i>
+          Name:
+          {{ user.name }}
+        </div>
+        <div class="col">
+          <i class='bx bx-envelope bx-sm'></i>
+          Email :
+          {{ user.email }}
+        </div>
+      </div>
+      <div>
+        <i class='bx bx-calendar-heart bx-sm'></i>
+        Joined at :
+        {{ getJoinedAt }}
+      </div>
+      <div class="row">
+        <div class="col">
+          <i class='bx bx-trip bx-sm'></i>
+          Trips Count :
+          <nuxt-link to="/dashboard/trips">
+            {{ user.trips.length }}
+          </nuxt-link>
+        </div>
+        <div class="col">
+          <i class='bx bx-calculator bx-sm' ></i>
+          Days in trip : {{ getTotalTripsDuration }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment';
 export default {
-  layout:'dashboard'
+  layout:'dashboard',
+  data(){
+    return{
+      user:null,
+      is_loading:true
+    }
+  },
+  mounted() {
+    this.$api.get('/user')
+      .then(response=>this.user=response.data)
+      .finally(()=>{ this.is_loading=false })
+  },
+  computed:{
+    getTotalTripsDuration(){
+      let response=0
+      this.user.trips.forEach(trip=>{
+        let differentInDay =moment.duration(moment(trip.end).diff(moment(trip.start))).asDays()+1
+        response+=differentInDay
+      })
+      return response
+    },
+    getJoinedAt(){
+      return moment(this.user.created_at).format('DD-MMM-YYYY')
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
--- pages/dashboard/trips.vue
+++ pages/dashboard/trips.vue
@@ -1,15 +1,63 @@
 <template>
-  <div class="text-light">
-    trips
+  <div class="grid gap-3">
+    <div class="d-flex justify-content-center mt-1 mb-1" v-if="is_loading">
+      <div class="spinner-border" role="status"></div>
+    </div>
+    <table class="table table-hover table-dark" v-else-if="trips.length>0">
+      <thead>
+      <tr>
+        <th scope="col"> </th>
+        <th scope="col">Start date</th>
+        <th scope="col">End data</th>
+        <th scope="col">City</th>
+        <th scope="col">Hotel</th>
+        <th scope="col"> </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(trip, index) in trips">
+        <td>{{ index+1 }}</td>
+        <td>{{ trip.start }}</td>
+        <td>{{ trip.end }}</td>
+        <td>
+          <nuxt-link :to="'/cities/'+trip.room.hotel.city.name">
+            {{ trip.room.hotel.city.name }}
+          </nuxt-link>
+        </td>
+        <td>
+          <nuxt-link :to="'/hotels/'+trip.room.hotel.id">
+            {{ trip.room.hotel.name }}
+          </nuxt-link>
+        </td>
+        <td>
+          <nuxt-link class="btn btn-warning" :to="'/trips/'+trip.id">
+            Details
+          </nuxt-link>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+    <div class="d-flex justify-content-center mt-2 mb-2" v-else>
+      <h1 class="font-bold">
+        You don't have Trip
+      </h1>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  layout:'dashboard'
+  layout:'dashboard',
+  data(){
+    return{
+      trips:[],
+      is_loading:true
+    }
+  },
+  mounted() {
+    this.$api.get('/trips')
+      .then(response=>this.trips=response.data)
+      .finally(()=>{ this.is_loading=false })
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
