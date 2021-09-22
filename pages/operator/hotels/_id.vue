c5e098df25558ce7c4954a3dd39518db2e041512
Omid Reza <snip77.orh@gmail.com>
2021-09-22 23:13:10 +0330

    changelogs: 1. Create Hotel operator dashboard page to select hotel 2. Create Hotel operator hotel page (not complete) TODO: 1. complete hotel page for hotel operator

---
+++ pages/operator/hotels/_id.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style="color: white">
+    Helllooooo
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ pages/operator/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="d-grid gap-3 container">
+    <span class="h3 text-light">
+      Hotels which have access :
+    </span>
+    <div class="row justify-around">
+      <div class="col-3 text-light" v-for="hotel in hotels">
+        <Hotel :hotel="hotel"></Hotel>
+        <div>
+          <nuxt-link class="btn btn-success mb-3 panel-btn" :to="'hotels/'+hotel.id">Hotel Panel</nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      hotels:null
+    }
+  },
+  mounted() {
+    this.$nextTick(()=>{
+      this.$api.get('/operator/hotels')
+        .then(response => this.hotels=response.data)
+    })
+  }
+}
+</script>
+
+<style scoped>
+.panel-btn{
+  width: 100%;
+}
+</style>
