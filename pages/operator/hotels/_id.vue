5e80f8dd1973ee3e4553f4b36be4875e0937c982
Omid Reza <snip77.orh@gmail.com>
2021-09-25 12:47:12 +0330

    Send request to server to get hotel info in hotel page (operator)

--- pages/operator/hotels/_id.vue
+++ pages/operator/hotels/_id.vue
@@ -1,11 +1,22 @@
 <template>
   <div style="color: white">
-    Helllooooo
+
   </div>
 </template>
 
 <script>
 export default {
-  middleware: 'operator'
+  middleware: 'operator',
+  data(){
+    return{
+      hotel:null
+    }
+  },
+  mounted() {
+    this.$nextTick(()=>{
+      this.$api.get('/operator/hotels/'+this.$route.params.id)
+        .then(response => this.hotel=response.data)
+    })
+  }
 }
 </script>
